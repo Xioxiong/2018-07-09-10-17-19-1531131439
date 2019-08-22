af64a8687fcb4102ed1f7cbe897183e6c1ae8ada
xiongwei <2894001817@qq.com>
2019-08-22 21:54:43 +0800

    完成练习二

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,10 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    private String klass;
+
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class 2.";
+    }
 }
